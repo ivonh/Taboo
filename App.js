2ccd9a28983920cf57421be917f29cbed6fbe987
Ivon Harris <Audiivon@gmail.com>
2022-05-05 15:10:58 +1000

    Owner

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        LETS FUCKING SMASH THIS
+      <Image source ={require('./assets/LOGO.png')} />
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
