36a41bee8ddf27ec03a810c2a4b6bdf2d64a79eb
Ivon Harris <Audiivon@gmail.com>
2022-05-06 14:32:04 +1000

    loginscreen

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, ImageBackground, View } from "react-native";
+import { StyleSheet, ImageBackground, View, Image, Text } from "react-native";
 import React from "react";
 
 function WelcomeScreen(props) {
@@ -7,9 +7,11 @@ function WelcomeScreen(props) {
       style={styles.background}
       source={require("../assets/background.jpg")}
     >
-      <Image source={require("../assets/LOGO.png")} />
-      <View style={styles.loginButton}></View>
-      <View style={styles.registerButton}></View>
+      <Image style={styles.logo} source={require("../assets/LOGO.png")} />
+      <Text style={styles.title}>Period care that cares</Text>
+      <Text style={styles.paragraph}> This is a paragrpah</Text>
+      <View style={styles.loginButton} />
+      <View style={styles.registerButton} />
     </ImageBackground>
   );
 }
@@ -18,16 +20,41 @@ const styles = StyleSheet.create({
   background: {
     flex: 1,
     justifyContent: "flex-end",
+    alignItems: "center",
   },
   loginButton: {
-    width: " 100%",
+    width: "100",
     height: 70,
-    backColor: " #fc5c65",
+    backgroundColor: "#fc5c65",
+    justifyContent: "flex-end",
   },
   registerButton: {
     width: " 100%",
     height: 70,
-    backColor: " #4ecdc4",
+    backgroundColor: "#4ecdc4",
+    justifyContent: "flex-end",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    position: "absolute",
+    top: 50,
+  },
+  title: {
+    flex: 1,
+    color: "pink",
+    fontFamily: "Ebrima",
+    fontSize: 34,
+    position: "absolute",
+    top: 180,
+  },
+  paragraph: {
+    flex: 1,
+    color: "pink",
+    fontFamily: "Arial",
+    fontSize: 34,
+    position: "absolute",
+    top: 240,
   },
 });
 
